50bcf24308a4c8bf3779931c344dd43022b73a24
Álvaro <adepablos@edu.tecnocampus.com>
2020-12-06 04:53:56 +0100

    gameManager, coins, power, lifes

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,14 +4,58 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField] public int lifes = 3;
+    private int power = 8;
+    private int coins = 0;
 
     public delegate void RestartGameDelegate();
-
     public static event RestartGameDelegate RestartGameEvent;
+
+    
 
     public void RestartGame()
     {
         RestartGameEvent.Invoke();
+    }
+
+    public int Power
+    {
+        get { return power; }
+    }
+
+    public void plusPower(int n)
+    {
+        if (power + n >= 8) power = 8;
+        if (power + n <= 0)
+        {
+            power = 0;
+            Die();
+        }
+        power += n;
+    }
+
+    public int Coins
+    {
+        get { return coins; }
+    }
+
+    public void plusCoins(int n)
+    {
+        if (coins + n <= 0) coins = 0;
+        coins += n;
+    }
+
+    public void Die()
+    {
+        //Die mario animation
+        if (lifes > 0)
+        {
+            //ShowDeathMenu
+        }
+        else
+        {
+            //End Game
+        }
     }
 
     private void Update()
