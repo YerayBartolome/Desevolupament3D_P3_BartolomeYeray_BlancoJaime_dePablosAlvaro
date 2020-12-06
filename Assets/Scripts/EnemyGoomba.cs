25db495dd0918e965098f417d03288268f58910b
Álvaro <adepablos@edu.tecnocampus.com>
2020-12-06 21:09:48 +0100

    Goomba stuff

---
+++ Assets/Scripts/EnemyGoomba.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyGoomba : MonoBehaviour, EnemyInterface
+{
+    Animator animator;
+
+   
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public int getEnemyDamage()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void TakeHit()
+    {
+        throw new System.NotImplementedException();
+    }
+}
