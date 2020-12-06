0fa23319b601893d364c2f3ad33de97333c7c8e5
Álvaro <adepablos@edu.tecnocampus.com>
2020-12-06 03:52:04 +0100

    Pushable
    Need to add changes at scene for this to work

---
+++ Assets/Scripts/CollisionController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionController : MonoBehaviour
+{
+
+    [SerializeField] float pushPower = 2;
+
+    void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        Rigidbody body = hit.collider.attachedRigidbody;
+
+
+        if (body == null || body.isKinematic) { return; }
+
+
+        if(hit.gameObject.layer == LayerMask.NameToLayer("Pushable"))
+        {
+            body.AddForceAtPosition(Vector3.down * pushPower, hit.point);
+        }
+    }
+}
