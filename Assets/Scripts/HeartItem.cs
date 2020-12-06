014ca04b391eeaad848b334ccddf719baa84af44
Álvaro <adepablos@edu.tecnocampus.com>
2020-12-06 19:16:25 +0100

    coins, hearts and light

---
+++ Assets/Scripts/HeartItem.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeartItem :MonoBehaviour, ItemInterface
+{
+    [SerializeField] Transform gameManagerObject;
+    private GameManager gameManager;
+
+    private void Start()
+    {
+        gameManager = gameManagerObject.GetComponent<GameManager>();
+    }
+    public void get()
+    {
+        gameManager.plusPower(1);
+        Debug.Log("get power");
+        Destroy(transform.parent.gameObject);
+    }
+}
